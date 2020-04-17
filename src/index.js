2393aae9522f74758eee1e8bfff9dcabbdccbca1
AliceRamirez17 <alicemarcelaramirez@gmail.com>
2020-04-16 21:20:03 -0500

    pruebas

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import { example } from './example.js';
+import firebase from 'firebase';
 
 example();
+
+const botonRegistrar = document.getElementById('boton');
+
+
+botonRegistrar.addEventListener('click', () => {
+
+  const email = document.getElementById('email').value;
+  const contrasena = document.getElementById('contrasena').value;
+
+  firebase.auth().createUserWithEmailAndPassword(email, contrasena).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+    // ...
+  });
+});
