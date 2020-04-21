d2145f3cdd40c87dcc7d839f2f7366a24a2ba710
AliceRamirez17 <alicemarcelaramirez@gmail.com>
2020-04-21 00:45:00 -0500

    Estilos para la primera HU

--- src/view/signin.js
+++ src/view/signin.js
@@ -2,15 +2,16 @@ import { signIn } from '../firebase-controller.js';
 
 export default () => {
     const viewSignIn = document.createElement('div');
+    viewSignIn.classList.add('signin');
     viewSignIn.innerHTML = `
-    <p class="description">Bienvenida a la red social para mujeres</p>
-
-    <input id="emailLogIn" type="email" placeholder="e-mail">
-    <input id="passwordLogIn" type="password" placeholder="contraseña">
-    <button id="btnInitSession"><a href="#/home">Iniciar sesión</a></button>
-
-    <p>¿No tienes cuenta?</p>
-    <button id="btnViewSignUp"><a href="#/signup">Regístrate</a></button>`;
+    <img src="../img/logo.svg" alt="Voz Amiga" class="logo-social-network">
+    <p class="text">Bienvenida a la red social para mujeres</p>
+    <input class="email-login" id="emailLogIn" type="email" placeholder="e-mail" autocomplete="off">
+    <input class="password-login" id="passwordLogIn" type="password" placeholder="contraseña" autocomplete="off">
+    <button class="btn-initsession" id="btnInitSession"><a href="#/home">Iniciar sesión</a></button>
+    <p class="text2">o ingresa con</p>
+    <p class="text2">¿No tienes cuenta?</p>
+    <button class="btn-signup" id="btnViewSignUp"><a href="#/signup">Regístrate</a></button>`;
 
     const btnLogIn = viewSignIn.querySelector('#btnInitSession');
     btnLogIn.addEventListener('click', () => {
