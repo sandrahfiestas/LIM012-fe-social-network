8bc4661520f33250bf76e63bea0671dfd366bea6
sandrahfiestas <shfiestas@gmail.com>
2020-04-23 09:43:49 -0500

    Validando datos ingreso

--- src/view/register.js
+++ src/view/register.js
@@ -45,3 +45,58 @@ export default () => {
 
     return viewSignUp;
 };
+
+
+/* 
+const btnNewAccount = document.querySelector('#btnNewAccount');
+const nameUser = document.querySelector ('#nameUser');
+const emailLogUp = document.querySelector('#emailSignUp');
+const passwordLogUp = document.querySelector('#passwordSignUp');
+const alerta = document.querySelector ('#alerta');
+
+const allowText = (eventType, allowed) => {
+  console.log(eventType,allowed);
+  // 8=backSpace, 46=Supr, 37=flecha izquierda, 39=flecha derecha
+  let characters = "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+  let specialKeys = [8, 37, 39, 46];
+  // Seleccionar los caracteres a partir del parametro de la función
+  switch (allowed) {
+    case 'car':
+      allowed = characters;
+      break;
+  }
+  // Tecla pulsada
+  let evento = eventType || window.event;
+  console.log(evento);
+  let characterCode = evento.charCode || evento.keyCode;
+  console.log(characterCode);
+  let character = String.fromCharCode(characterCode);
+  console.log(character);
+  // Comprobar si la tecla pulsada es alguna de las teclas_especiales
+ let specialKey = false;
+ for(let i in specialKeys) {
+   if(characterCode == specialKeys[i]) {
+     specialKey = true;
+     break;
+   }
+ }
+ // Comprobar si la tecla pulsada es un caracter permitido o si es una tecla especial
+ return allowed.indexOf(character) != -1 || specialKey;
+}
+
+
+btnNewAccount.addEventListener('click', () => {
+  // Comprueba campos vacios
+if (nameUser.value == "" || emailLogUp.value == "" || passwordLogUp.value =="") {
+   nameUser.classList.add('invalid');
+   emailLogUp.classList.add('invalid');
+   passwordLogUp.classList.add('invalid');
+   alerta.innerHTML = 'Campos vacios. Ingrese información';
+   // return false;
+}else{
+  alert('Enviando formulario');
+}
+
+});
+
+*/
