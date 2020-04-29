ce6de3f7da3c24b7969f20f42d6f815397b86af3
sandrahfiestas <shfiestas@gmail.com>
2020-04-29 13:14:02 -0500

    Crear perfil de usuario con datos de Google. Y validación de campos en blanco en el registro del usuario.

--- src/view/register.js
+++ src/view/register.js
@@ -31,89 +31,23 @@ export default () => {
         <a class="text-init-session" id="btnViewLogIn" href="#/signin">Inicia sesión</a>
     </div>`;
 
-  // const nameUser = viewSignUp.querySelector('#nameUser');
-  // const emailLogUp2 = viewSignUp.querySelector('#emailSignUp');
-  // const passwordLogUp2 = viewSignUp.querySelector('#passwordSignUp');
-  const btnNewAccount = viewSignUp.querySelector('#btnNewAccount');
-  // const alertIconN = viewSignUp.querySelector('#alertIconN');
-  // const alertIconE = viewSignUp.querySelector('#alertIconE');
-  // const alertIconP = viewSignUp.querySelector('#alertIconP');
-
-
-
-/*
-  // Validación de formulario
-
-  nameUser.addEventListener('input', () => {
-    const patronName = /^[A-Za-z]+$/;
-    const coincideName = patronName.test(nameUser.value);
-    if (coincideName) {
-      //alertIconN.classList.add('hide');
-      nameUser.classList.remove('inputInvalid');
-      // btnNewAccount.classList.remove('btn-locked');
-      // console.log('verdadero usuario');
-    } else {
-      //alertIconN.classList.remove('hide');
-      //alertIconN.classList.add('iconInvalid');
-      nameUser.classList.add('inputInvalid');
-      btnNewAccount.classList.add('btn-locked');
-      // console.log('falso boton usuario');
-    }
-  });
-
-  emailLogUp2.addEventListener('input', () => {
-    const patronEmail = /[A-Za-z0-9]+@[a-z]+\.[a-z]+/;
-    const coincideEmail = patronEmail.test(emailLogUp2.value);
-    if (coincideEmail) {
-      // alertIconE.classList.add('hide');
-      emailLogUp2.classList.remove('inputInvalid');
-      // btnNewAccount.classList.remove('btn-locked');
-      // console.log('verdadero email');
-    } else {
-      //alertIconE.classList.remove('hide');
-      //alertIconE.classList.add('iconInvalid');
-      emailLogUp2.classList.add('inputInvalid');
-      btnNewAccount.classList.add('btn-locked');
-      // console.log('falso boton email');
-    }
-  });
-
-    const patronPassword = /[A-Za-z0-9]{6,20}$/;
-    const coincidenciaPassword = patronPassword.test(passwordLogUp2.value);
-    if (coincidenciaPassword ) {
-      //alertIconP.classList.add('hide');
-      passwordLogUp2.classList.remove('inputInvalid');
-      btnNewAccount.classList.remove('btn-locked');
-      btnNewAccount.disabled = false;
-      // console.log('verdadero password');
-    } else {
-      //alertIconP.classList.remove('hide');
-      //alertIconP.classList.add('iconInvalid');
-      passwordLogUp2.classList.add('inputInvalid');
-      btnNewAccount.classList.add('btn-locked');
-      // console.log('falso boton password');
-    }
-    */
-
+  
 const nameUser = viewSignUp.querySelector('#nameUser');
 const emailLogUp2 = viewSignUp.querySelector('#emailSignUp');
 const passwordLogUp2 = viewSignUp.querySelector('#passwordSignUp');
+const btnNewAccount = viewSignUp.querySelector('#btnNewAccount');
 
-
+// Inicia validación de registro
   nameUser.addEventListener('input', () => {  
     if (nameUser.value == "" || emailLogUp2.value == "" || passwordLogUp2.value == ""){
       btnNewAccount.classList.add('btn-locked');
       btnNewAccount.disabled = true;
-      console.log('email o password vacios');
     }else if (nameUser.validity.valid && emailLogUp2.validity.valid && passwordLogUp2.validity.valid){
       btnNewAccount.classList.remove('btn-locked');
       btnNewAccount.disabled = false;
-      console.log('activar boton');
-      console.log(nameUser.validity.valid);
     } else {
       btnNewAccount.classList.add('btn-locked');
       btnNewAccount.disabled = true;
-      console.log('desactivar boton');
     }
   });
 
@@ -121,16 +55,12 @@ const passwordLogUp2 = viewSignUp.querySelector('#passwordSignUp');
     if (nameUser.value == "" || emailLogUp2.value == "" || passwordLogUp2.value == ""){
       btnNewAccount.classList.add('btn-locked');
       btnNewAccount.disabled = true;
-      console.log('usuario o password vacios');
     } else if (nameUser.validity.valid && emailLogUp2.validity.valid && passwordLogUp2.validity.valid) {
       btnNewAccount.classList.remove('btn-locked');
       btnNewAccount.disabled = false;
-      console.log('activar boton');
-      console.log(emailLogUp2.validity.valid);
     } else {
       btnNewAccount.classList.add('btn-locked');
       btnNewAccount.disabled = true;
-      console.log('desactivar boton');
     }
   });
 
@@ -138,21 +68,15 @@ const passwordLogUp2 = viewSignUp.querySelector('#passwordSignUp');
     if (nameUser.value == "" || emailLogUp2.value == "" || passwordLogUp2.value == ""){
       btnNewAccount.classList.add('btn-locked');
       btnNewAccount.disabled = true;
-      console.log('usuario o password vacios');
     } else if (nameUser.validity.valid && emailLogUp2.validity.valid && passwordLogUp2.validity.valid) {
       btnNewAccount.classList.remove('btn-locked');
       btnNewAccount.disabled = false;
-      console.log('activar boton');
-      console.log(passwordLogUp2.validity.valid);
     } else {
       btnNewAccount.classList.add('btn-locked');
       btnNewAccount.disabled = true;
-      console.log('desactivar boton');
     }
   });
-  // Termina validación de formulario
-
-
+  // Termina validación de registro
 
   btnNewAccount.addEventListener('click', () => {
     const emailLogUp = viewSignUp.querySelector('#emailSignUp').value;
