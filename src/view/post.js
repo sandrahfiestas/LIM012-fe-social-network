138ba030968ad0ebae541ce782f45ee2388e7252
AliceRamirez17 <alicemarcelaramirez@gmail.com>
2020-05-05 10:17:12 -0500

    Corrigiendo css

--- src/view/post.js
+++ src/view/post.js
@@ -3,20 +3,19 @@ import { deletePost } from '../firestore-controller.js';
 
 export const eachPost = (objNote) => {
   const eachNote = document.createElement('div');
+  eachNote.classList.add('each-post');
   eachNote.innerHTML = `
-    <div class="each-post">
     <p>${objNote.name}</p>
     <p>${objNote.post}</p>
     <div class="container-menu-post">
         <input type="checkbox" id="menu-post" class="hide">
         <label for="menu-post" class="label-menu-post"></label>
-        <nav class="" id="nav-post">
+        <nav class="hide" id="nav-post">
         <ul class="menu-post">
             <li class="btn-post-edit" id="btnPostEdit">Editar</li>
             <li class="btn-post-delete" id="delete-${objNote.id}">Eliminar</li>
         </ul>
         </nav>
-    </div>
     </div>`;
 
   const menuPost = eachNote.querySelector('#menu-post');
